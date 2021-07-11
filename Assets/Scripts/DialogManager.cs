475a0ac84dd37b617243e6af95e613fe8d01b170
Allen Solomon <ars190009@utdallas.edu>
2021-07-11 00:01:08 -0500

    Ver 2

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    public Text dialogText;
+    public Text nameText;
+    public GameObject dialogBox;
+    public GameObject nameBox;
+
+    public string[] dialogLines;
+
+    public int currentLine;
+    private bool justStarted;
+
+    public static DialogManager instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        //dialogText.text = dialogLines[currentLine];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(dialogBox.activeInHierarchy)
+        {
+            if(Input.GetButtonUp("Fire1"))
+            {
+                if (!justStarted)
+                {
+                    currentLine++;
+
+                    if (currentLine >= dialogLines.Length)
+                    {
+                        dialogBox.SetActive(false);
+                        PlayerController.instance.canMove = true;
+                    }
+                    else
+                    {
+                        dialogText.text = dialogLines[currentLine];
+                    }
+                }
+                else
+                {
+                    justStarted = false;
+                }
+                                    
+                }
+
+                
+            }
+        }
+    
+
+    public void ShowDialog(string[] newLines) 
+    {
+        dialogLines = newLines;
+
+        currentLine = 0;
+
+        dialogText.text = dialogLines[0];
+        dialogBox.SetActive(true);
+        justStarted = true;
+
+        PlayerController.instance.canMove = false;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
     public string areaTransitionName;
     private Vector3 bottomLeftLimit;
     private Vector3 topRightLimit;
+
+    public bool canMove = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,15 +31,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxis("Vertical")) * moveSpeed;
-
+        if (canMove)
+        {
+            theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxis("Vertical")) * moveSpeed;
+            
+        }
+        else
+        {
+            theRB.velocity = Vector2.zero;
+        }
         myAnim.SetFloat("moveX", theRB.velocity.x);
         myAnim.SetFloat("moveY", theRB.velocity.y);
 
-        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+            {
+            if (canMove)
             {
                 myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
                 myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+            }
+                
             }
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x), Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y), transform.position.z);
     }
